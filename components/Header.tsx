89859bdf210a144384c60cc5ef390a5d05aad9d1
alerta-360 <alerta360santacruz@gmail.com>
2025-09-30 01:54:07 -0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import alerta360Logo from '/alerta360.jpeg';
+import uepgLogo from '/uepg.jpeg';
 
 export const Header: React.FC = () => {
     return (
         <header className="bg-blue-900 text-white p-4 shadow-md flex items-center justify-between z-50">
             <div className="flex items-center">
-                <img src="/alerta360.jpeg" alt="Alerta360 Logo" className="h-10 w-auto" />
+                <img src={alerta360Logo} alt="Alerta360 Logo" className="h-10 w-auto" />
                 <h1 className="text-2xl font-bold ml-3">Alerta360</h1>
             </div>
-            <img src="/uepg.jpeg" alt="Logo UEP Gloria" className="h-12 w-12 rounded-full object-cover" />
+            <img src={uepgLogo} alt="Logo UEP Gloria" className="h-12 w-12 rounded-full object-cover" />
         </header>
     );
 };
